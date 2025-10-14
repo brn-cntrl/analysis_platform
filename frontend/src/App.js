2611cfe19720d05544b37bae57fb98c140f0f294
Brian Cantrell <brncntrl@Brians-MBP.attlocal.net>
2025-10-14 10:55:38 -0700

    Added new tab functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import AnalysisViewer from './components/AnalysisViewer';
+import ResultsViewer from './components/ResultsViewer';
 
 function App() {
-  return (
-    <div className="App">
-      <AnalysisViewer />
-    </div>
-  );
+  // Simple routing based on URL path
+  const path = window.location.pathname;
+  
+  if (path === '/results') {
+    return <ResultsViewer />;
+  }
+  
+  return <AnalysisViewer />;
 }
 
 export default App;
